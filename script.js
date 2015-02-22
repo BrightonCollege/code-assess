3f173e1775135e4fb29d6a0d093af102cd04fd1d
adrianiainlam <adrianiainlam@gmail.com>
2015-02-22 17:38:27 +0000

    fix

--- script.js
+++ script.js
@@ -54,8 +54,7 @@ function processResults(jsonObj, count) {
     var q = q_array[count];
     var gist = id("gist");
     
-    var gisthtml = '<html><body onload="parent.adjustIframeSize(document.body.scrollHeight)">'
-                    + '<script src="https://gist.github.com/adrianiainlam/2bfe1b8e70616eada426.js"></script></body></html>';
+    var gisthtml = '<html><body><script src="https://gist.github.com/adrianiainlam/2bfe1b8e70616eada426.js"></script></body></html>';
     gist.contentDocument.open();
     gist.contentDocument.writeln(gisthtml);
     gist.contentDocument.close();
