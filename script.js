aae98c216ee6c2cef4d4697012d54318494b3f38
adrianiainlam <adrianiainlam@gmail.com>
2015-02-22 14:14:04 +0000

    script

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function ChangeURL(URL) {
     document.getElementById("Main").className = "ModeHidden"
     document.getElementById("Game").className = "ModeVisible";
 }
+
+function getJsonRequest(url) {
+    var req = new XMLHttpRequest();
+    var jsonObj;
+    req.open("GET", url);
+    req.onreadystatechange = function() {
+        if(this.readyState == 4) {
+            jsonObj = JSON.parse(this.response);
+            processResults(jsonObj);
+        }
+    }
+    req.send();
+}
+
+function processResults(jsonObj) {
+    // TODO: replace with something else
+    console.log(jsonObj);
+}
