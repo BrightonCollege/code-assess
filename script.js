a48eec7af01822c2b161cac8c8bd594ac7108732
adrianiainlam <adrianiainlam@gmail.com>
2015-02-23 01:53:26 +0000

    Code readability + fixes. See commit history for details.

--- script.js
+++ script.js
@@ -1,80 +1,90 @@
 var score = 0;
-var time = 0;
 var timer;
 var LastMode; //will hold last mode choosen for the retry button, URL if custom
-var Questions = new Array() //used in JSON parcing
-function ChangeMode(Mode) {
-    document.getElementById("Main").className = "ModeHidden";
-    document.getElementById("GameOver").className = "ModeHidden";
-    if (Mode == 'Java') {
-        LastMode = 'Java';
-        document.getElementById("URL").innerText = "Questions/JavaBeginnerExampleQuestion.json";
-        getJsonRequest("Questions/JavaBeginnerExampleQuestion.json");
+var Questions = []; //used in JSON parsing
+
+var id = document.getElementById.bind(document); // makes lines shorter and more readable
+
+function ChangeMode(mode) {
+    hideElement(id("Main"));
+    hideElement(id("GameOver"));
+    switch(mode) {
+        case "Player":
+            showElement(id("custom"));
+            break;
+        case "Creator":
+            showElement(id("creator"));
+            break;
+        case "AccessURL":
+            getJsonRequest(id("userURL").value);
+            break;
+        /* Since there is currently no known way of determining the <difficulty>,
+           this part is commented out. When the <difficulty> can be known,
+           this should be reactivated and the subsequent temporary workaround
+           cases should be removed. */
+        /*default:
+            showElement(id("Game"));
+            LastMode = mode;
+            var location = "Questions/" + mode + "<difficulty>" + "ExampleQuestion.json";
+            id("URL").innerText = location;
+            getJsonRequest(location);
+            break;*/
+        case "Java":
+            LastMode = "Java";
+            showElement(id("Game"));
+            id("URL").innerText = "Questions/JavaBeginnerExampleQuestion.json";
+            getJsonRequest("Questions/JavaBeginnerExampleQuestion.json");
+            break;
+        case "Python":
+            LastMode = 'Python';
+            showElement(id("Game"));
+            id("URL").innerText = "Questions/PythonNormalExampleQuestion.json";
+            getJsonRequest("Questions/PythonListComprehensionQs.json");
+            break;
     }
-    if (Mode == 'Python') {
-        LastMode = 'Python';
-        document.getElementById("URL").innerText = "Questions/PythonNormalExampleQuestion.json";
-        getJsonRequest("Questions/PythonListComprehensionQs.json");
-    }
-    if (Mode == 'Player') {
-        document.getElementById("custom").className = "ModeVisible";
-        return;
-    }
-    if (Mode == 'Creator') {
-        document.getElementById("creator").className = "ModeVisible";
-        return;
-    }
-    if (Mode == 'AccessURL') {
-            getJsonRequest(document.getElementById("userURL").value);
-            return;
-        }
-    document.getElementById("Game").className = "ModeVisible";
 }
 
 function goHome() {
     clearInterval(timer);
-    document.getElementById("Main").className = "ModeVisible";
-    document.getElementById("Game").className = "ModeHidden";
-    document.getElementById("custom").className = "ModeHidden";
-    document.getElementById("GameOver").className = "ModeHidden";
-    document.getElementById("creator").className = "ModeHidden";
+    showElement(id("Main"));
+    hideElement(id("Game"));
+    hideElement(id("custom"));
+    hideElement(id("GameOver"));
+    hideElement(id("creator"));
 }
 
 function GameOverScreen() {
-    document.getElementById("Game").className = "ModeHidden";
-    document.getElementById("finalScore").innerHTML = score;
-    document.getElementById("finalTime").innerHTML = time;
-    document.getElementById("GameOver").className = "ModeVisible";
+    hideElement(id("Game"));
+    id("finalScore").innerHTML = score;
+    id("finalTime").innerHTML = id("time").innerHTML;
+    showElement(id("GameOver"));
 }
 
 function getJsonRequest(url) {
     var req = new XMLHttpRequest();
-    var jsonObj;
     req.open("GET", url);
     req.onreadystatechange = function() {
         if(this.readyState == 4) {
             if(this.status == 404) { // or != 200, depending on what you want
-                console.log("not found"); // change here
-                document.getElementById("wrongURL").style.display = "block"; 
-                return;
+                showElement(id("wrongURL"));
+            } else {
+                showElement(id("Game"));
+                hideElement(id("custom"));
+                score = 0;
+                id("time").innerHTML = 0;
+                var updateTimer = function() {
+                    id("time").innerHTML = parseInt(id("time").innerHTML) + 1;
+                };
+                timer = setInterval(updateTimer, 1000);
+                processResults(JSON.parse(this.response), 0);
             }
-            document.getElementById("Game").className = "ModeVisible";
-            document.getElementById("custom").className = "ModeHidden";
-            jsonObj = JSON.parse(this.response);
-            score = 0;
-            time = 0;
-            document.getElementById("time").innerHTML = 0;
-            timer = setInterval(updateTimer, 1000);
-            processResults(jsonObj, 0);
         }
     }
-    document.getElementById("wrongURL").hidden = true;  
+    hideElement(id("wrongURL"));
     req.send();
 }
 
 function processResults(jsonObj, count) {
-    var id = document.getElementById.bind(document);
-    var embedScript = id("embedScript");
     var q_array = jsonObj.Questions;
     if (count == q_array.length) {
         //Show game over screen
@@ -91,11 +101,9 @@ function processResults(jsonObj, count) {
     gist.contentDocument.writeln(gisthtml);
     gist.contentDocument.close();
     
-    
     updateScore();
     
     var ans_array = q.Answers;
-    
     var correctAnswer = ans_array[4];
     
     id("Answer1").innerHTML = ans_array[0];
@@ -103,40 +111,41 @@ function processResults(jsonObj, count) {
     id("Answer3").innerHTML = ans_array[2];
     id("Answer4").innerHTML = ans_array[3];
 
-    for (i = 1; i < 5; i++) {
+    for (i = 1; i <= 4; i++) {
         if (i == correctAnswer) {
             id("Answer" + i).onclick = function () {
                 score++;
-                processResults(jsonObj, count+1);
+                processResults(jsonObj, count + 1);
             };
         } else {
             id("Answer" + i).onclick = function () {
                 score--;
-                processResults(jsonObj, count+1);
+                processResults(jsonObj, count + 1);
             };
         }
     }
 }
 
-  function parseUserChoiceToJson() {
-        var question = new Object();
-        question.QuestionText = document.getElementById("questionURL").value;
-        question.Answers = new Array(5);
-        for (i = 1; i < 5; i++) {
-            question.Answers[i-1] = document.getElementById("variant" + i).value;
-        }    
-        question.Answers[4] = document.getElementById("rightVariant").value;
-        Questions.push(question);
-        document.getElementById("JSON").innerHTML = '{"Questions":' +JSON.stringify(Questions) + '}';
-    }
-
-
-function updateTimer() {
-    time++;
-    document.getElementById("time").innerHTML = time;
+function parseUserChoiceToJson() {
+    var question = new Object();
+    question.QuestionText = id("questionURL").value;
+    question.Answers = [];
+    for (i = 1; i <= 4; i++) {
+        question.Answers.push(id("variant" + i).value);
+    }    
+    question.Answers.push(id("rightVariant").value);
+    Questions.push(question);
+    id("JSON").innerHTML = '{"Questions":' + JSON.stringify(Questions) + '}';
 }
 
-
 function updateScore() {
-    document.getElementById("score").innerHTML = score;
+    id("score").innerHTML = score;
+}
+
+function showElement(elem) {
+    elem.style.display = "block";
+}
+
+function hideElement(elem) {
+    elem.style.display = "none";
 }
