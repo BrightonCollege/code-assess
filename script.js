7efc51e9240ca8079efcf257149b5cf683e5a66b
MaksymPetyak <Petyak.MI@gmail.com>
2015-02-23 00:41:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -55,6 +55,7 @@ function getJsonRequest(url) {
         if(this.readyState == 4) {
             if(this.status == 404) { // or != 200, depending on what you want
                 console.log("not found"); // change here
+                document.getElementById("wrongURL").hidden = false; 
                 return;
             }
             document.getElementById("Game").className = "ModeVisible";
@@ -67,7 +68,7 @@ function getJsonRequest(url) {
             processResults(jsonObj, 0);
         }
     }
-    document.getElementById("wrongURL").hidden = false;  
+    document.getElementById("wrongURL").hidden = true;  
     req.send();
 }
 
